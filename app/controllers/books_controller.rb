6c4addae9f20b72f94d0a41e67c218a60ed3bbb4
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-02-10 10:47:17 -0600

    Updated controllers with error message and index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,16 @@
 class BooksController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        books = Book.all
+        render json: books
+    end
+
+    private
+
+    def render_not_found_response
+        render json: {error: "Look at that, it appears we don't have that book!"}, status: :not_found 
+    end
+
 end
--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -1,2 +1,15 @@
 class WizardsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    wizard = Wizard.all
+    render json: wizard
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Wizard not found. Hmmm, I wonder if they apparated somewhere!"}, status: :not_found
+  end
+  
 end
