1ee54a10565e753e4f411376f36bce5e4b16ddc8
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-02-14 15:35:11 -0600

    added create to wizard and book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
-
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+wrap_parameters format: []
 
     def index
         books = Book.all
@@ -12,7 +12,16 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
         render json: book
     end
 
+    def create
+        book = Book.create(book_params)
+        render json: book, status: :created
+    end
+
     private
+
+    def book_params
+        params.permit(:name, :description)
+    end
 
     def render_not_found_response
         render json: {error: "Look at that, it appears we don't have that book!"}, status: :not_found 
--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -1,9 +1,9 @@
 class WizardsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  wrap_parameters format: []
 
   def index
-    wizard = Wizard.all
-    render json: wizard
+    render json: Wizard.all
   end
 
   def show
@@ -11,7 +11,16 @@ class WizardsController < ApplicationController
     render json: wizard
   end
 
+  def create
+    wizard = Wizard.create(wizard_params)
+    render json: wizard, status: :created
+  end
+
   private
+
+  def wizard_params
+    params.permit(:first, :last, :house)
+  end
 
   def render_not_found_response
     render json: { error: "Wizard not found. Hmmm, I wonder if they apparated somewhere!"}, status: :not_found
